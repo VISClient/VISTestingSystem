227ffebae782000465d4a2be326aafd5ebc5fe61
sofatitovass <sof_titova@rambler.ru>
2017-05-03 22:02:12 +0300

    attemts table fixed / time moved to trash

--- web/attempts/attempt.js
+++ web/attempts/attempt.js
@@ -20,9 +20,9 @@
 
    .controller('AttemptCtrl', function ($scope){ // testing version
     $scope.atts = [
-      {"time": "10", "task": "A", "attempt": "1", "result": "WA 17"},
-      {"time": "30", "task": "A", "attempt": "2", "result": "OK"},
-      {"time": "20", "task": "B", "attempt": "1", "result": "OK"}
+      {"task": "A", "attempt": "1", "result": "WA 17"},
+      {"task": "A", "attempt": "2", "result": "OK"},
+      {"task": "B", "attempt": "1", "result": "OK"}
     
     ];
 });
