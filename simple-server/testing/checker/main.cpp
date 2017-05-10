e3a9377f94a8d8e8939476be07bd942b59be07d5
Ivan <guzev1997@gmail.com>
2017-05-11 02:08:57 +0300

    Add files via upload

--- simple-server/testing/checker/main.cpp
+++ simple-server/testing/checker/main.cpp
@@ -7,40 +7,28 @@
 
 using namespace std;
 
-int main(int argc, char * argv[]) {
-    pid_t pid = fork();
-    string command = "echo " + to_string(pid) + " > /sys/fs/cgroup/memory/group0/tasks";
-    int l = system(command.c_str());
-
-    char * a = argv[1];
-    //int n = atoi(argv[2]);
-    string str = a;
-    string new_str = "";
-    for (int i = 0; i < str.size(); i++) {
-        if (str[i] == '.') {
-            break;
-        } else {
-            new_str = new_str + str[i];
-        }
-    }
-    string tmp = new_str;
-    new_str = "g++ " + str + " -o " + new_str + ".exe";
-    int i = system(new_str.c_str());
-    str = "./" + tmp + ".exe";
-    //start contestian program
+int main(int argc, char *argv[]) {
+    string str = "g++ -std=c++11 testing.cpp -o testing.exe";
+    int i = system(str.c_str());
+    str = "./testing.exe";
+    int returned_sign = 0;
     time_t start = time(0);
-    i = execl(str.c_str(), NULL);
-    double seconds_since_start = difftime(time(0), start);
-    if (seconds_since_start - 2.0 > 0.00001) {
-        cout << "TL ";
-        exit(0);
-    }
-    // stop contestian program
-    str = "./checker.exe input.txt output.txt ans.txt";
-    i = system(str.c_str());
-    if (i == 0) {
-        cout << "OK";
+    pid_t pid = fork();
+    if (pid == 0) {
+        int own_pid = getpid();
+        system("echo " + to_string(own_pid) + " > /sys/fs/cgroup/memory/group0/tasks");
+        system("echo " + to_string(own_pid) + " > /sys/fs/cgroup/cpu/group0/tasks");
+        returned_sign = execl(str.c_str(), NULL);
+        exit(returned_sign);
     } else {
-        cout << "WA";
+        wait();
+        double seconds_since_start = difftime(time(0), start);
+        if (seconds_since_start - 2.0 > 0.00001) {
+            cout << "TL ";
+            exit(2);
+        }
+        str = "./checker.exe input.txt output.txt ans.txt";
+        i = system(str.c_str());
+        return i;
     }
 }
